f1726fea2351da0cd8b37287455fba37b277a75e
Sweet Ali <sweetali0520@gmail.com>
2026-06-17 15:37:25 +0600

    check notification

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,3 +1,4 @@
+
 import axios from "axios";
 import { toast } from "sonner";
 
@@ -6,22 +7,65 @@ const client = axios.create({
   withCredentials: true,
 });
 
+let isRefreshing = false;
+let failedQueue: any[] = []
+
+const processQueue = (error:any, success = false) => {
+  failedQueue.forEach(prom => {
+    if(success) prom.resolve();
+    else prom.reject(error);
+  });
+
+  failedQueue = [];
+}
 client.interceptors.response.use(
   (res) => res,
-  (error) => {
+  async(error) => {
     // console.log(error.response.data)
-    console.log(error.response.data)
-    const message = error.response.data.message || "Something went wrong!";
-    if (error.response.status === 401) {
-      if (typeof window !== "undefined") {
-        toast.error(message);
-        const pathname = window.location.pathname
+    const originalRequest = error.config;
+    const message = error.response?.data?.message || "Something went wrong!";
+    
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      if(isRefreshing){
+        return new Promise((resolve, reject) => {
+          failedQueue.push({resolve, reject})
+        })
+        .then(() => client(originalRequest))
+        .catch(err => Promise.reject(err))
+      }
+
+       isRefreshing = true;
+
+      try{
+        await axios.post(
+          'https://voiceerp.mapleitfirm.com/api/auth/refresh',
+           {},
+            { withCredentials: true }
+          );
+          processQueue(null,true);
+
+          return client(originalRequest);
+      }
+      catch(refreshError){
+          processQueue(refreshError, false);
+          if (typeof window !== "undefined") {
+            toast.error("Session expired. Please login again.");
+
+        const pathname = window.location.pathname;
+
         if ((pathname !== "/login") && (pathname !== "/verify-forget-otp")) {
           window.location.href = "/login";
         }
       }
-      return Promise.reject(error);
+       return Promise.reject(refreshError);
+      }
+      finally{
+        isRefreshing = false;
+      }
     }
+
     toast.error(message);
     return Promise.reject(error);
   },
